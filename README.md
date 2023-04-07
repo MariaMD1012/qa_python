3d5d474f99af9ed4d5d440ded912842cee00c41d
Maria Getmanova <maria.rthefam@yandex.ru>
2023-04-08 00:38:30 +0300

    sprint2_new_commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # qa_python
+
+# test_add_book test_add_rating_to_absent_book_fails 
+# Нельзя выставить рейтинг книге, которой нет в списке test_cant_set_rating_greater_than_ten 
+# Нельзя выставить рейтинг меньше 1 test_cant_set_rating_greater_than_ten 
+# Нельзя выставить рейтинг больше 10 test_absent_book_has_no_rating 
+# У не добавленной книги нет рейтинга Добавление книги в избранное 
+# def test_add_to_favorites Нельзя добавить книгу в избранное, если её нет в словаре 
+# test_add_to_favorites_fails_if_not_in_ratings Проверка удаления книги из избранного 
+# test_delete_from_favorites(удалить из избранного) Проверка получения списка избранных книг 
+# test_get_list_of_favorites_books.На получение книг с определенным рейтингом завершается неудачей, если рейтинг неверный
--- tests.py
+++ tests.py
@@ -1,97 +1,76 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    class TestBooksCollector:
-        def test_add_book(books_collector):
-            books_collector.add_new_book(NAME)
-            assert books_collector.favorites == []
-            assert books_collector.books_rating == {NAME: 1}
+    def test_add_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Убить пересмешника')
+        assert collector.favorites == []
+        assert collector.books_rating == {'Бесы': 1}
 
+    def test_add_rating_to_absent_book_fails(self):
+        collector = BooksCollector()
+        collector.add_new_book('Приключения Буратино')
+        collector.set_book_rating('Буратино', 5)
+        assert collector.favorites == []
+        assert collector.books_rating == {'Властелин колец': 1}
 
+    def test_cant_set_rating_less_than_one(self):
+        collector = BooksCollector()
+        collector.add_new_book('Унесенные ветром')
+        collector.set_book_rating('Зеленая миля', 0)
+        assert collector.favorites == []
+        assert collector.books_rating == {'Побег из шоушенка': 1}
 
+    def test_cant_set_rating_greater_than_ten(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюны')
+        collector.set_book_rating('Три мушкетера', 11)
+        assert collector.favorites == []
+        assert collector.books_rating == {'Граф Монте-Кристо': 1}
 
-def books_collector():
-    return BooksCollector()
+    def test_absent_book_has_no_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алиса в стране чудес')
+        rating = collector.get_book_rating('Двадцать лет спустя')
+        assert rating is None
 
-    NAME = 'Book Name'
-    WRONG_NAME = 'Wrong Name'
+    def test_add_to_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Мастер и Маргарита')
+        collector.add_book_in_favorites('Королева Марго')
+        assert collector.favorites == ['Отцы и дети']
+        assert collector.books_rating == {'Муму': 1}
 
+    def test_add_to_favorites_fails_if_not_in_ratings(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Гранатовый браслет')
+        assert collector.favorites == []
+        assert collector.books_rating == {}
 
-    class TestBooksCollector:
-        def test_add_rating_to_absent_book_fails(books_collector):
-             books_collector.add_new_book(NAME)
-             books_collector.set_book_rating(WRONG_NAME, 5)
-             assert books_collector.favorites == []
-             assert books_collector.books_rating == {NAME: 1}
+    def test_delete_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Вишневый сад')
+        collector.add_book_in_favorites('Евгений Онегин')
+        collector.delete_book_from_favorites('Идиот')
+        assert collector.favorites == []
+        assert collector.books_rating == {'Август Четырнадцатого': 1}
 
-    class TestBooksCollector:
-        def test_cant_set_rating_less_than_one(books_collector):
-            books_collector.add_new_book(NAME)
-            books_collector.set_book_rating(NAME, 0)
-            assert books_collector.favorites == []
-            assert books_collector.books_rating == {NAME: 1}
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Братья Карамазовы')
+        collector.add_book_in_favorites('Война и мир')
+        assert collector.get_list_of_favorites_books() == ['Анна Каренина']
 
-    class TestBooksCollector:
-        def test_cant_set_rating_greater_than_ten(books_collector):
-            books_collector.add_new_book(NAME)
-            books_collector.set_book_rating(NAME, 11)
-            assert books_collector.favorites == []
-            assert books_collector.books_rating == {NAME: 1}
-
-    class TestBooksCollector:
-        def test_absent_book_has_no_rating(books_collector):
-            books_collector.add_new_book(NAME)
-            rating = books_collector.get_book_rating(WRONG_NAME)
-            assert rating is None
-
-    class TestBooksCollector:
-        def test_add_to_favorites(books_collector):
-            books_collector.add_new_book(NAME)
-            books_collector.add_book_in_favorites(NAME)
-            assert books_collector.favorites == [NAME]
-            assert books_collector.books_rating == {NAME: 1}
-
-    class TestBooksCollector:
-        def test_add_to_favorites_fails_if_not_in_ratings(books_collector):
-            books_collector.add_book_in_favorites(NAME)
-            assert books_collector.favorites == []
-            assert books_collector.books_rating == {}
-
-    class TestBooksCollector:
-        def test_delete_from_favorites(books_collector):
-            books_collector.add_new_book(NAME)
-            books_collector.add_book_in_favorites(NAME)
-            books_collector.delete_book_from_favorites(NAME)
-            assert books_collector.favorites == []
-            assert books_collector.books_rating == {NAME: 1}
-
-    class TestBooksCollector:
-        def test_get_list_of_favorites_books(books_collector):
-            books_collector.add_new_book(NAME)
-            books_collector.add_book_in_favorites(NAME)
-            assert books_collector.get_list_of_favorites_books() == [NAME]
-
-    class TestBooksCollector:
-        def test_get_books_with_specific_rating_fails_if_wrong_rating(books_collector):
-            books_collector.add_new_book(NAME)
-            result = books_collector.get_books_with_specific_rating(0)
-            assert [] == result
+    def test_get_books_with_specific_rating_fails_if_wrong_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Мёртвые души')
+        result = collector.get_books_with_specific_rating(0)
+        assert [] == result
