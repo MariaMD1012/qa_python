95de3988af4bb25852784a42019dd87026851f90
Maria Getmanova <maria.rthefam@yandex.ru>
2023-04-09 17:44:02 +0300

    sprint2_new_commit_part2

--- tests.py
+++ tests.py
@@ -11,8 +11,7 @@ class TestBooksCollector:
 
     def test_add_book(self):
         collector = BooksCollector()
-        collector.add_new_book('Убить пересмешника')
-        assert collector.favorites == []
+        collector.add_new_book('Бесы')
         assert collector.books_rating == {'Бесы': 1}
 
     def test_add_rating_to_absent_book_fails(self):
@@ -20,21 +19,20 @@ class TestBooksCollector:
         collector.add_new_book('Приключения Буратино')
         collector.set_book_rating('Буратино', 5)
         assert collector.favorites == []
-        assert collector.books_rating == {'Властелин колец': 1}
+        assert collector.books_rating == {'Приключения Буратино': 1}
 
     def test_cant_set_rating_less_than_one(self):
         collector = BooksCollector()
         collector.add_new_book('Унесенные ветром')
-        collector.set_book_rating('Зеленая миля', 0)
-        assert collector.favorites == []
-        assert collector.books_rating == {'Побег из шоушенка': 1}
+        collector.set_book_rating('Унесенные ветром', 0)
+        assert collector.books_rating == {'Унесенные ветром': 1}
 
     def test_cant_set_rating_greater_than_ten(self):
         collector = BooksCollector()
         collector.add_new_book('Дюны')
-        collector.set_book_rating('Три мушкетера', 11)
+        collector.set_book_rating('Дюны', 11)
         assert collector.favorites == []
-        assert collector.books_rating == {'Граф Монте-Кристо': 1}
+        assert collector.books_rating == {'Дюны': 1}
 
     def test_absent_book_has_no_rating(self):
         collector = BooksCollector()
@@ -46,8 +44,8 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('Мастер и Маргарита')
         collector.add_book_in_favorites('Королева Марго')
-        assert collector.favorites == ['Отцы и дети']
-        assert collector.books_rating == {'Муму': 1}
+        collector.favorites == ['Королева Марго']
+        assert collector.books_rating == {'Мастер и Маргарита': 1}
 
     def test_add_to_favorites_fails_if_not_in_ratings(self):
         collector = BooksCollector()
@@ -61,13 +59,12 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Евгений Онегин')
         collector.delete_book_from_favorites('Идиот')
         assert collector.favorites == []
-        assert collector.books_rating == {'Август Четырнадцатого': 1}
 
     def test_get_list_of_favorites_books(self):
         collector = BooksCollector()
         collector.add_new_book('Братья Карамазовы')
         collector.add_book_in_favorites('Война и мир')
-        assert collector.get_list_of_favorites_books() == ['Анна Каренина']
+        collector.get_list_of_favorites_books() == ['Братья Карамазовы']
 
     def test_get_books_with_specific_rating_fails_if_wrong_rating(self):
         collector = BooksCollector()
